08ea4a988bd04c0d8ac4c3443b22c3ce3e26cc8f
kim-jaun <67224239+kim-jaun@users.noreply.github.com>
2021-04-05 17:45:52 +0900

    210405_study

--- gretting.js
+++ gretting.js
@@ -1,6 +1,36 @@
 const form = document.querySelector(".js-form"),
-  input = form.querySelector("input");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-function init() {}
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEnventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
 
 init();
